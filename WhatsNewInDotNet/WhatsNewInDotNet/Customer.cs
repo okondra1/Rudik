4fd5c280ceb74995cdfd34d7f5eb8f2e2615560c
okondra1 <117862827+okondra1@users.noreply.github.com>
2023-11-13 12:46:52 -0500

    Initial commit

---
+++ WhatsNewInDotNet/WhatsNewInDotNet/Customer.cs
@@ -0,0 +1,43 @@
+﻿
+// File Scoped Namespace
+namespace WhatsNewInDotNet;
+
+public record Customer
+{
+    public Customer(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+
+    // entities
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    public required string EmailAddress { get; init; } = string.Empty;
+
+
+
+    public Payment GetLastPayment()
+    {
+        // go to the database, whatever.
+        return new Payment(532.52M, DateTime.Now.AddDays(-3));
+
+    }
+
+}
+
+
+//public class Payment
+//{
+//    public Payment(decimal amount, DateTime date)
+//    {
+//        Amount = amount;
+//        Date = date;
+//    }
+//    public decimal Amount { get; private set; }
+//    public DateTime Date { get; private set; }
+//}
+
+public record Payment(decimal Amount, DateTime Date);
---
+++ WhatsNewInDotNet/WhatsNewInDotNet/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using WhatsNewInDotNet;
+
+
+Console.WriteLine("Hello, World!");
+
+var bob = new Customer(18, "Bob Smith")
+{
+    EmailAddress = "bob@aol.com"
+};
+
+var bob2 = new Customer(18, "Bob Smith")
+{
+    EmailAddress = "bob@aol.com"
+};
+
+if (bob == bob2)
+{
+    Console.WriteLine("They are the same!");
+}
+else
+{
+    Console.WriteLine("They are not the same");
+}
+
+
+
+Console.WriteLine(bob.Name.ToUpper());
+
+var p = bob.GetLastPayment();
+
+Console.WriteLine($"Bob made a payment of {p.Amount:c} on {p.Date:m}");
+
+
+
+
